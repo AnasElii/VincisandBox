47f55d69367e7629be264c7fc198fbdcc9e36cc9
AninossII <aninosspro@gmail.com>
2022-10-26 18:59:38 +0100

    Java PJ_01 Update

--- Java/Pj_01/src/App.java
+++ Java/Pj_01/src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tapez le jour, le moi et l'annee de naissance");
+        int jour = sc.nextInt();
+        int moi = sc.nextInt();
+        int annee = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Tapez le nome");
+        String nome = sc.nextLine();
+
+        System.out.println("BonJour M." + nome + " Votre date de naissance et: " + jour + "/" + moi + "/" + annee);
     }
 }
