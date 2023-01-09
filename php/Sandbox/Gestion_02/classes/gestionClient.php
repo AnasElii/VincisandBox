47117e05b1e3de8adb68e1a00c09ca7f147a1375
AninossII <aninosspro@gmail.com>
2023-01-09 21:03:38 +0000

    Gestion CLient gESTION 2

--- php/Sandbox/Gestion_02/classes/gestionClient.php
+++ php/Sandbox/Gestion_02/classes/gestionClient.php
@@ -1,36 +1,16 @@
 <?php // Gestion Client
 
-
-// include "../classes/client.php";
-// include "../data/fileManager.php";
 require_once "../data/dbConnection.php";
 
 class GestionClient
 {
-    private $clientList = array();
-
-    function insert()
+    public function insert()
     {
-        // $clientList = array();
-        // $clientList = unSerializeList();
-        // $client = new Client(0, "anas", "Hey Dalia 699", "0661829837");
-
-        try {
-            $query = "INSERT INTO client (null, 'anas', 'hey dalia' , :tel)";
-            // $list_vlaues = array(
-            //     "nom" => "anas",
-            //     "adress" => "Hey Dalia 699",
-            //     "tel" => "0661829837"
-            // );
-            $requete = $pdo->query($query);
-             $err = $e->getMessage();
-            echo $err;
-        }
-
-
-        // array_push($clientList, $client);
-        // $client = new Client(0, "mouad", "Hey Dalia 699", "066114699367");
-        // array_push($clientList, $client);
-        // serializeList($clientList);
+        $db = new Database();
+        $db->execQuery("INSERT INTO client VALUE (null, 'ikram', 'hey dalia' , '0661829837')");
+        echo "<br>New client added successfully!";
     }
 }
+
+$g = new GestionClient();
+$g->insert();
--- php/Sandbox/Gestion_02/data/dbConnection.php
+++ php/Sandbox/Gestion_02/data/dbConnection.php
@@ -1,13 +1,38 @@
-<?php
-$host = "localhost";
-$port = "3306";
-$data = "commercial";
-$user = "root";
-$pass = "1234";
-$chrs = "utf8mb4";
-$attr = "mysql:host=$host;dbname=$data;charset=$chrs";
-$opts = [
-    PDO::ATTR_AUTOCOMMIT         => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+<?php //Db Connection 
+
+class Database
+{
+    private $host = "localhost";
+    private $port = "3306";
+    private $data = "commercial";
+    private $user = "root";
+    private $pass = "1234";
+    private $chrs = "utf8mb4";
+    private $opts =
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+
+    private $pdo;
+
+    public function __construct()
+    {
+        try {
+            $this->pdo = new PDO("mysql:host=$this->host;port=$this->port;dbname=$this->data;charset=$this->chrs", $this->user, $this->pass, $this->opts);
+            echo "Connection Established";
+        } catch (Exception $ex) {
+            print "Erro!: " . $ex->getMessage() . "<br>";
+            die();
+        }
+    }
+
+    public final function execQuery($q)
+    {
+        $query = $this->pdo->prepare($q);
+        $query->execute();
+        $result = $query->fetchAll();
+        print_r($result);
+    }
+}
