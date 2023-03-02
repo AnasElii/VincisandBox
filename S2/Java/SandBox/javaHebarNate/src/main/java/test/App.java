8479f0810a069861127a1efe5244ad2330096b32
AninossII <aninosspro@gmail.com>
2023-03-02 00:39:07 +0000

    Jv Hn Fixed

---
+++ S2/Java/SandBox/javaHebarNate/src/main/java/test/App.java
@@ -0,0 +1,32 @@
+package test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.*;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+  public static void main(String[] args) {
+
+    // Create SessionFactory
+    Configuration configuration = new Configuration().configure();
+    StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+        .applySettings(configuration.getProperties());
+    SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
+
+    // Create Session
+    Session session = sessionFactory.openSession();
+
+    // Start a Transaction
+    Transaction transaction = session.beginTransaction();
+
+    // CRUD
+    Produit p = new Produit();
+    session.save(p);
+
+    // Destroy The Session
+    transaction.commit();
+    session.close();
+  }
+}
